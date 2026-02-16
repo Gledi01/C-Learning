9c8453d76a6b95a1da78dd8eb70870e91545b2f4
Wayan Gledy <162592892+Gledi01@users.noreply.github.com>
2026-02-17 00:51:31 +0800

    Create tebak_angka.cpp

---
+++ src/06_mini_project/tebak_angka.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int angka = 7, tebak;
+    cin >> tebak;
+
+    if(tebak == angka)
+        cout << "Benar";
+    else
+        cout << "Salah";
+    return 0;
+}
