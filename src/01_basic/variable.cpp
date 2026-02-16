d3a3de3a9f43f64fbdafb70fecd3947c0304738d
Wayan Gledy <162592892+Gledi01@users.noreply.github.com>
2026-02-17 00:38:08 +0800

    Create variable.cpp

---
+++ src/01_basic/variable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int umur = 18;
+    float tinggi = 170.5;
+    cout << "Umur: " << umur << endl;
+    cout << "Tinggi: " << tinggi << endl;
+    return 0;
+}
