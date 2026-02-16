3f6a99629ff38e2325e5c3f0a6952490e022eaf7
Wayan Gledy <162592892+Gledi01@users.noreply.github.com>
2026-02-17 00:39:03 +0800

    Create operasi_aritmatika.cpp

---
+++ src/01_basic/operasi_aritmatika.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 5;
+    cout << "Tambah: " << a + b << endl;
+    cout << "Kurang: " << a - b << endl;
+    cout << "Kali: " << a * b << endl;
+    cout << "Bagi: " << a / b << endl;
+    return 0;
+}
