9e1f38eacd98062160f2996cbb6dd1b6fbae7464
Wayan Gledy <162592892+Gledi01@users.noreply.github.com>
2026-02-17 00:40:02 +0800

    Create if_else.cpp

---
+++ src/02_control_flow/if_else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int nilai;
+    cin >> nilai;
+
+    if (nilai >= 75)
+        cout << "Lulus";
+    else
+        cout << "Tidak Lulus";
+
+    return 0;
+}
